0910379bdc3ae563abbb88c446c14f82a7f950a1
adityaxkullu <kulshruadityaa@gmail.com>
2026-07-18 13:24:03 +0530

    Time: 4 ms (41.26%), Space: 31.2 MB (25.47%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
@@ -2,14 +2,42 @@ class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
         int n = nums.size();
+        vector<int> ans;
 
-        for(int i = 0; i < n; i++) {
-            nums[i] *= nums[i];
+        int right = 0;
+        // find first non-negative element
+        while(right < n && nums[right] < 0) {
+            right++;
+        }
+        // last negative element
+        int left = right - 1;
+        
+        // merge the two sorted parts
+        while(left >= 0 && right < n) {
+            int sqLeft = nums[left] * nums[left];
+            int sqRight = nums[right] * nums[right];
+
+            if(sqLeft <= sqRight) {
+                ans.push_back(sqLeft);
+                left--;
+            }else {
+                ans.push_back(sqRight);
+                right++;
+            } 
+        }
+  
+        // remaining negative numbers
+        while(left >= 0) {
+            ans.push_back(nums[left] * nums[left]);
+            left--;
         }
 
-        sort(nums.begin(), nums.end());
+        // remaining positive numbers
+        while(right < n) {
+            ans.push_back(nums[right] * nums[right]);
+            right++;
+        }
 
-        return nums;
-        
+        return ans;   
     }
 };
