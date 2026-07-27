31109f40ff36a8da473253ef4fd108e1813c7ef4
adityaxkullu <kulshruadityaa@gmail.com>
2026-07-27 14:02:24 +0530

    Time: 0 ms (100%), Space: 11.4 MB (65.35%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,28 +1,21 @@
 class Solution {
 public:
-    bool compare(vector<int> & window, vector<int> & need) {
-        for(int i = 0; i < 256; i++) {
-            if(window[i] < need[i]) {
-                return false;
-            }
-        }
-        return true;
-    }
     string minWindow(string s, string t) {
-        int m = s.length();
-        int n = t.length();
-        int low = 0, start, res = m + 1;
-        vector<int> window(256, 0);
-        vector<int> need(256, 0);
+        vector<int> freq(128, 0);
 
-        for(int i = 0; i < n; i++) {
-            need[t[i]]++;
+        for(int i = 0; i < t.length(); i++) {
+            freq[t[i]]++;
         }
+        int low = 0, start = 0, res = s.length() + 1;
+        int count = t.length();
 
-        for(int high = 0; high < m; high++) {
-            window[s[high]]++;
+        for(int high = 0; high < s.length(); high++) {
+            if(freq[s[high]] > 0) {
+                count--;
+            }
+            freq[s[high]]--;
 
-            while(compare(window, need)) {
+            while(count == 0) {
                 int len = high - low + 1;
 
                 if(res > len) {
@@ -30,14 +23,20 @@ public:
                     start = low;
                 }
 
-                window[s[low]]--;
+                freq[s[low]]++;
+
+                if(freq[s[low]] > 0) {
+                    count++;
+                }
+
                 low++;
+               
             }
+
         }
 
-        return s.substr(start, res);
+        return (res == s.length() + 1) ? "" : s.substr(start, res);
 
 
-        
     }
 };
