753e1894d2f3d51aefd4dbda050867a25bf9f0e7
adityaxkullu <kulshruadityaa@gmail.com>
2026-08-04 22:58:22 +0530

    Time: 0 ms (100%), Space: 10 MB (97.76%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int rob(vector<int>& a) {
+       int n = a.size();
+
+       if(n == 1) return a[0];
+       
+       int prev2 = a[0];
+       int prev1 = max(a[0], a[1]);
+
+       for(int i = 2; i < n; i++) {
+        int curr = max(prev1, prev2 + a[i]);
+
+        prev2 = prev1;
+        prev1 = curr;
+
+       }
+
+       return prev1;
+    }
+};
