8e0679c7f4915f1ed8e44268c15edf678c55a702
adityaxkullu <kulshruadityaa@gmail.com>
2026-08-11 22:38:28 +0530

    Time: 1 ms (54.72%), Space: 22.9 MB (60.14%) - LeetHub

--- 0986-interval-list-intersections/0986-interval-list-intersections.cpp
+++ 0986-interval-list-intersections/0986-interval-list-intersections.cpp
@@ -5,34 +5,21 @@ public:
         int m = b.size();
 
         vector<vector<int>> ans;
-        int i = 0, j = 0, s, e; 
+        int i = 0, j = 0; 
 
         while(i < n && j < m) {
-            int start1 = a[i][0], end1 = a[i][1];
-            int start2 = b[j][0], end2 = b[j][1];
+            int start = max(a[i][0], b[j][0]);
+            int end = min(a[i][1], b[j][1]);
 
-            if(start1 <= start2) {
-                if(end1 >= start2) {
-                    s = max(start1, start2);
-                    e = min(end1, end2);
-
-                    ans.push_back({s, e});
-                }
-            }else {
-                if(end2 >= start1) {
-                    s = max(start1, start2);
-                    e = min(end1, end2);
-
-                    ans.push_back({s, e}); 
-                }
+            if(start <= end) {
+                ans.push_back({start, end});
             }
 
-            if(end1 <= end2) {
+            if(a[i][1] <= b[j][1]) {
                 i++;
             }else {
                 j++;
             }
-
         }
 
         return ans;
