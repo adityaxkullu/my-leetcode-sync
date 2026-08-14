ed888d8a3fcd4e927985e36600db03aeb9b7089e
adityaxkullu <kulshruadityaa@gmail.com>
2026-08-14 18:33:15 +0530

    Time: 0 ms (100%), Space: 8.6 MB (64.53%) - LeetHub

--- 2259-remove-digit-from-number-to-maximize-result/2259-remove-digit-from-number-to-maximize-result.cpp
+++ 2259-remove-digit-from-number-to-maximize-result/2259-remove-digit-from-number-to-maximize-result.cpp
@@ -3,21 +3,31 @@ public:
     string removeDigit(string num, char k) {
         int n = num.size();
 
-        for(int i = 0; i < n - 1; i++) {
-            if(num[i] == k && num[i + 1] > k) {
-                return num.substr(0, i) + num.substr(i + 1);
-            }
-        }
+        stack<int> s;
+        int removeIdx = -1;
 
-        int pos;
-        for(int i = n - 1; i >= 0; i--) {
+        for(int i = 0; i < n; i++) {
             if(num[i] == k) {
-            pos = i;
-            break;
+                if(i + 1 < n && num[i + 1] > k) {
+                    removeIdx = i;
+                    break;
+                }
+
+                removeIdx = i;
+            }
+
+            s.push(num[i]);
+        }
+
+        string ans;
+
+        for(int i = 0; i < n; i++) {
+            if(i != removeIdx) {
+                ans.push_back(num[i]);
             }
         }
 
-        return num.substr(0, pos) + num.substr(pos + 1);
+        return ans;
     
     }
 };
