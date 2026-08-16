569754377796593ec0c2459003a3a0b02f2cc0f2
adityaxkullu <kulshruadityaa@gmail.com>
2026-08-16 14:14:35 +0530

    Time: 0 ms (100%), Space: 12.4 MB (12.3%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool match(unordered_map<char, int> need, unordered_map<char, int> have) {
+        for(auto i : need) {
+            char c = i.first;
+            int fneed = i.second;
+            int fhave = have[c];
+
+            if(fhave < fneed) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+    bool canConstruct(string ransomNote, string magazine) {
+        unordered_map<char, int> need;
+        unordered_map<char, int> have;
+
+        for(int i = 0; i < ransomNote.size(); i++) {
+            need[ransomNote[i]]++;
+        }
+
+        for(int i = 0; i < magazine.size(); i++) {
+            have[magazine[i]]++;
+        }
+
+        return match(need, have);
+        
+    }
+};
