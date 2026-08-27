6e6c5d38567d8a7fec002367985c3ac8aebb63ec
adityaxkullu <kulshruadityaa@gmail.com>
2026-08-27 11:16:30 +0530

    Time: 245 ms (5%), Space: 61.6 MB (97.16%) - LeetHub

--- 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
+++ 0973-k-closest-points-to-origin/0973-k-closest-points-to-origin.cpp
@@ -5,22 +5,39 @@ public:
         return d;
     }
 
-    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
-        priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-
-        for(int i = 0; i < points.size(); i++) {
-            int dist = findDist(points[i]);
-            pq.push({dist, i});
-        }
-
-        vector<vector<int>> res;
-        while(k--) {
-            res.push_back(points[pq.top().second]);
-            pq.pop();
-        }
-
-        return res;
+    int partition(vector<vector<int>> &points, int low, int high) {
+        int pivotDist = findDist(points[high]);
         
+        int i = low;
+        for(int j = low; j < high; j++) {
+            if(findDist(points[j]) <= pivotDist) {
+                swap(points[i], points[j]);
+                i++;
+            }
+        }
+
+        swap(points[i], points[high]);
+
+        return i;
+    }
+
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int k) {
+       int low = 0, high = points.size() - 1;
+
+       while(low <= high) {
+        int pivotIdx = partition(points, low, high);
+
+        if(pivotIdx == k - 1) break;
+
+        if(pivotIdx > k - 1) {
+            high = pivotIdx - 1;
+        }else {
+            low = pivotIdx + 1;
+        }
+
+       }
+
+       return vector<vector<int>>(points.begin(), points.begin() + k);
         
     }
 };
