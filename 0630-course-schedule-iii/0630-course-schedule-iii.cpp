24c74bc67177a46f674b1196aa29797af4e26cd9
adityaxkullu <kulshruadityaa@gmail.com>
2026-08-29 18:15:29 +0530

    Time: 59 ms (23.89%), Space: 67.6 MB (26.61%) - LeetHub

---
+++ 0630-course-schedule-iii/0630-course-schedule-iii.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    struct cmp {
+        bool operator() (vector<int> &a, vector<int> &b) {
+            return a[1] < b[1];
+        }
+    };
+
+    int scheduleCourse(vector<vector<int>>& courses) {
+        int n = courses.size();
+        sort(courses.begin(), courses.end(), cmp());
+
+        priority_queue<int> pq;
+
+        int time = 0;
+
+        for(auto course : courses) {
+            int duration = course[0];
+            int deadline = course[1];
+
+            time += duration;
+            pq.push(duration);
+
+            if(time > deadline) {
+                time -= pq.top();
+                pq.pop();
+            }
+        }
+
+        return pq.size();
+
+    }
+};
