cd6e1e4859eb3b7eafaacf81572e427e017acf81
adityaxkullu <kulshruadityaa@gmail.com>
2026-09-03 12:29:51 +0530

    Time: 39 ms (12.64%), Space: 45.2 MB (9.48%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,47 +1,33 @@
 class Solution {
 public:
-    // set<vector<int>> s;
+    void solve(vector<int> &a, int idx, int sum, int target, vector<int> dairy, vector<vector<int>> &res) {
+        int n = a.size();
+        if(idx == n) {
+            if(sum == target) {
+                res.push_back(dairy);
+            }
 
-    void getAllCombination(vector<int>& arr, int target, int idx, vector<vector<int>>& ans,  vector<int>& combin) {
-        if(idx == arr.size() || target < 0) {
             return;
         }
 
-        if(target == 0) {
-            // if(s.find(combin) == s.end()){
-            //     ans.push_back(combin);
-            //     s.insert(combin);
-            // }
-            ans.push_back(combin);
-            return;
+        solve(a, idx + 1, sum, target, dairy, res);
+
+        if(a[idx] + sum <= target) {
+            dairy.push_back(a[idx]);
+            sum += a[idx];
+            
+            solve(a, idx, sum, target, dairy, res);
+
+            dairy.pop_back();
+            sum -= a[idx];
         }
-
-
-
-        combin.push_back(arr[idx]);
-
-        //single
-        // getAllCombination(arr, target - arr[idx], idx + 1, ans, combin);
-
-        // multiple
-        getAllCombination(arr, target - arr[idx], idx, ans, combin);
-        
-        // backtracking
-        combin.pop_back();
-
-        // exclusion
-        getAllCombination(arr, target, idx + 1, ans, combin);
-
-
     }
-   
-    
-    vector<vector<int>> combinationSum(vector<int>& arr, int target) {
-        vector<vector<int>> ans;
-        vector<int> combin;
-        
-        getAllCombination(arr, target, 0, ans, combin);
+    vector<vector<int>> combinationSum(vector<int>& a, int target) {
+        vector<vector<int>> res;
+        vector<int> dairy;
 
-        return ans;
+        solve(a, 0, 0, target, dairy, res);
+
+        return res;    
     }
 };
